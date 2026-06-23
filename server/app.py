54d0bc520ad11f539dff8c6774f7d3377f1d18de
munirasheikh-rgb <munira.sheikh@student.moringaschool.com>
2026-06-23 16:12:12 +0300

    building routes for car company

--- server/app.py
+++ server/app.py
@@ -1,1 +1,19 @@
+from flask import Flask
 existing_models = ['Beedle', 'Crossroads', 'M2', 'Panique']
+
+
+app = Flask( __name__ )
+
+@app.route("/")
+def index():
+    return "Welcome to Flatiron Cars"
+
+@app.route("/<model>")
+def model(model):
+    if model in existing_models:
+        return f"Flatiron {model} is in our fleet!"
+    else:
+        return f"No models called {model} exists in our catalog"
+
+if __name__ == "__main__":
+    app.run(port=5555,debug=True)
